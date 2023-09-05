f2165fdc41a35eb8d81ec31aafe0f4fb1abd2fe2
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-09-05 11:40:09 -0300

    Criando rota com o Router

--- ExercicioCpWeb/src/main.jsx
+++ ExercicioCpWeb/src/main.jsx
@@ -26,3 +26,9 @@ const router = createBrowserRouter([
   }
 ])
 
+React.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+)
+
