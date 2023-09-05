2c4f7bcaeffced492756c55b5c8866faf9dbd2fa
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-09-05 11:50:18 -0300

    Criando estrutura da função menu e importando no app

--- ExercicioCpWeb/src/components/Menu.jsx
+++ ExercicioCpWeb/src/components/Menu.jsx
@@ -4,7 +4,11 @@ export default function Menu(){
 
     return(
         <>
-        
+        <nav className="menu">
+            <Link to='/'>Home</Link>
+            <span> | </span>
+            <Link to='/produtos'>Produtos</Link>
+        </nav>
         </>
     )
 }
